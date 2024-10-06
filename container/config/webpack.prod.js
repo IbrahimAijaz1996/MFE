6694a830705ade2cf07bf4abcb39d9086e9e9871
ibrahim.aijaz1996 <ibrahim.aijaz1996@gmail.com>
2024-10-06 15:56:42 +0500

    SC

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -14,7 +14,7 @@ const prodConfig = {
         new moduleFederationPlugin({
             name: "container",
             remotes: {
-                marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+                marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
             },
             shared: packageJSON.dependencies,
         }),
