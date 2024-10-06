e8821670ffa8d2bbdf14a83f66dba90d7efaaea7
ibrahim.aijaz1996 <ibrahim.aijaz1996@gmail.com>
2024-10-06 22:48:21 +0500

    SC

--- marketing/src/app.js
+++ marketing/src/app.js
@@ -1,13 +1,17 @@
 import React from "react";
 import {Switch, Route, BrowserRouter} from "react-router-dom";
-import {StylesProvider} from "@material-ui/core/styles";
+import {StylesProvider, createGenerateClassName} from "@material-ui/core/styles";
 import Pricing from "./components/Pricing";
-import Landing from "./components/Landing"
+import Landing from "./components/Landing";
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: "ma",
+})
 
 export default () => {
     return (
         <div>
-            <StylesProvider>
+            <StylesProvider generateClassName={generateClassName}>
                 <BrowserRouter>
                     <Switch>
                         <Route exact path="/pricing" component={Pricing}/>
