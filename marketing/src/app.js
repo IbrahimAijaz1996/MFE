aa35589806cc348cf78f380e54c3a71324c9123c
ibrahim.aijaz1996 <ibrahim.aijaz1996@gmail.com>
2024-10-06 15:59:26 +0500

    SC

--- marketing/src/app.js
+++ marketing/src/app.js
@@ -1,7 +1,6 @@
 import React from "react";
 import {Switch, Route, BrowserRouter} from "react-router-dom";
 import {StylesProvider} from "@material-ui/core/styles";
-
 import Pricing from "./components/Pricing";
 import Landing from "./components/Landing"
 
